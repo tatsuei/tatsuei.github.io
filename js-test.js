e4b74c6a15959feba65ac9ee596d016821cb0828
tatsuei <eiji@tutanota.com>
2020-11-07 17:05:46 +0800

    Added a JS base64 decoder for testing

---
+++ js-test.js
@@ -0,0 +1,10 @@
+// Define the string
+var string = 'Hello World!';
+
+// Encode the String
+var encodedString = btoa(string);
+console.log(encodedString); // Outputs: "SGVsbG8gV29ybGQh"
+
+// Decode the String
+var decodedString = atob(encodedString);
+console.log(decodedString); // Outputs: "Hello World!"
